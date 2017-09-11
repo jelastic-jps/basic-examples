a12105499643c0a879839adaca97a2cd85be61c1
dzotic9 <dzotic9@gmail.com>
2017-09-11 11:59:57 +0300

    Update enableAutoScaling.js

--- automatic-horizontal-scaling/scripts/enableAutoScaling.js
+++ automatic-horizontal-scaling/scripts/enableAutoScaling.js
@@ -1,25 +1,60 @@
 var APPID = getParam("TARGET_APPID"),
+    QUOTA_MAX_NODES = "environment.maxsamenodescount",
+    VALUE_TYPE = "PERCENTAGES",
+    RESOURCE_TYPE = "CPU",
+    NODE_GROUP = "cp",
+    oTriggerActions,
+    nMaxNodesCount,
     oRespTurnOn,
-    oData;
+    aQuotas,
+    oData,
+    oResp;
+    
+    oTriggerActions = {
+        add: "ADD_NODE",
+        remove: "REMOVE_NODE"
+    };
+
+oResp = jelastic.billing.account.GetQuotas(appid, session);
+
+if (oResp.result != 0) {
+    return oResp;
+}
+
+aQuotas = oResp.array;
+
+for (var i = 0, n = aQuotas.length; i < n; i += 1) {
+
+    if (aQuotas[i].quota && aQuotas[i].quota.name == QUOTA_MAX_NODES) {
+        nMaxNodesCount = aQuotas[i].value;
+        break;
+    }
+}
+
+oResp = jelastic.env.trigger.GetTriggers(APPID, session, oTriggerActions.add);
+
+if (oResp.array.length > 0) {
+    oResp = jelastic.env.trigger.DeleteTrigger(APPID, session, oResp.array[0].id);
+}
 
 oData = {
-    "isEnabled": true,
-    "name": "hs-add-nginx",
-    "nodeGroup": "cp",
-    "period": 5,
-    "condition": {
-        "type": "GREATER",
-        "value": 80,
-        "resourceType": "CPU",
-        "valueType": "PERCENTAGES"
+    isEnabled: true,
+    name: "hs-add",
+    nodeGroup: NODE_GROUP,
+    period: 5,
+    condition: {
+        type: "GREATER",
+        value: 80,
+        resourceType: RESOURCE_TYPE,
+        valueType: VALUE_TYPE
     },
-    "actions": [
+    actions: [
         {
-            "type": "ADD_NODE",
-            "customData": {
-                "limit": 3,
-                "count": 1,
-                "notify": false
+            type: oTriggerActions.add,
+            customData: {
+                limit: nMaxNodesCount || 3,
+                count: 1,
+                notify: false
             }
         }
     ]
@@ -31,24 +66,31 @@ if (oRespTurnOn.result != 0) {
     return oRespTurnOn;
 }
 
+
+oResp = jelastic.env.trigger.GetTriggers(APPID, session, oTriggerActions.remove);
+
+if (oResp.array.length > 0) {
+    oResp = jelastic.env.trigger.DeleteTrigger(APPID, session, oResp.array[0].id);
+}
+
 oData = {
-    "isEnabled": true,
-    "name": "hs-remove-nginx",
-    "nodeGroup": "cp",
-    "period": 15,
-    "condition": {
-        "type": "LESS",
-        "value": 5,
-        "resourceType": "CPU",
-        "valueType": "PERCENTAGES"
+    isEnabled: true,
+    name: "hs-remove",
+    nodeGroup: NODE_GROUP,
+    period: 15,
+    condition: {
+        type: "LESS",
+        value: 5,
+        resourceType: RESOURCE_TYPE,
+        valueType: VALUE_TYPE
     },
-    "actions": [
+    actions: [
         {
-            "type": "REMOVE_NODE",
-            "customData": {
-                "limit": 2,
-                "count": 1,
-                "notify": false
+            type: oTriggerActions.remove,
+            customData: {
+                limit: 2,
+                count: 1,
+                notify: false
             }
         }
     ]

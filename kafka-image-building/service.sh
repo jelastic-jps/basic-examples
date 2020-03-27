d8cdee603c667627b02b461f9346ef67c5ad4c99
vlobzakov <39057925+vlobzakov@users.noreply.github.com>
2020-03-27 18:23:20 +0300

    Update service.sh

--- kafka-image-building/service.sh
+++ kafka-image-building/service.sh
@@ -6,15 +6,21 @@ cd kafka
 
 #Download prepared Dockerfile to build certified Kafka Broker image
 wget https://raw.githubusercontent.com/vlobzakov/basic-examples/master/kafka-image-building/Dockerfile -O Dockerfile
+echo
+echo "Docker file downloaded"
 
 #Create a directory tree where the system service files will be located. These files are added to the image due to the ADD src/. / line in Dockerfile.
 mkdir -p src/etc/sudoers.d src/etc/systemd/system  src/etc/jelastic src/var/lib/jelastic/overrides
+echo
+echo "Directory tree created"
 
 #Define start/stop service commands that are allowed to be executed as user kafka in src/etc/sudoers.d/kafka file.
 cat << EOF > src/etc/sudoers.d/kafka
 Cmnd_Alias KAFKA_SERVICE = /sbin/service kafka stop, /sbin/service kafka start, /sbin/service kafka restart
 %ssh-access ALL = NOPASSWD: KAFKA_SERVICE"
 EOF
+echo
+echo "Necessary rights granted for user kafka"
 
 #The image uses systemd to initialize multiple daemon services. Kafka distribution has ZooKeeper as an integrated component, and the two services are run in a single container. As a result, two appropriate systemd service files should be created (kafka.service and zookeeper.service).
 #kafka.service
@@ -33,6 +39,8 @@ Restart=on-abnormal
 [Install]
 WantedBy=multi-user.target
 EOF
+echo
+echo "kafka.service file created"
 
 #Create zookepeer.service
 cat << EOF > src/etc/systemd/system/zookeeper.service
@@ -51,6 +59,8 @@ Restart=on-abnormal
 [Install]
 WantedBy=multi-user.target
 EOF
+echo
+echo "zookeeper.service file created"
 
 #Create the src/etc/jelastic/favourites.conf file that configures shortcuts to the most common files and directories at the left pane of Jelastic Configuration File Manager.
 cat << EOF > src/etc/jelastic/favourites.conf
@@ -66,6 +76,8 @@ cat << EOF > src/etc/jelastic/favourites.conf
 [files]
 /home/jelastic/conf/variables.conf
 EOF
+echo
+echo "favourites.conf file created"
 
 #The redeploy.conf file lists files to keep during the container lifecycle.
 cat << EOF > src/etc/jelastic/redeploy.conf
@@ -78,6 +90,8 @@ cat << EOF > src/etc/jelastic/redeploy.conf
 /usr/lib/locale
 /etc/locale.conf
 EOF
+echo
+echo "redeploy.conf file created"
 
 #It is used by JEM to determine the template-specific logic (service initialization and restart in the case of Kafka).
 cat << EOF > src/var/lib/jelastic/overrides/envinfo.lib
@@ -89,3 +103,5 @@ kafka)
 ;;
 esac
 EOF
+echo
+echo "envinfo.lib file created"
